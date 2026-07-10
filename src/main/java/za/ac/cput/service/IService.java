e3f9ec025daa876f3c6cc4e31d9ba9474eb4e23b
Bongikazi Mnyamana <222718404@mycput.ac.za>
2026-07-10 13:12:31 +0200

    Add Payment Service implementation

--- src/main/java/za/ac/cput/service/IService.java
+++ src/main/java/za/ac/cput/service/IService.java
@@ -1,11 +1,12 @@
-package za.ac.cput.repository;
+
+package za.ac.cput.service;
 
 import java.util.List;
 
-public interface IRepository<T, ID> {
+public interface IService<T, ID> {
     T create(T t);
     T read(ID id);
     T update(T t);
-    boolean delete(ID t);
+    boolean delete(ID id);
     List<T> getAll();
 }
